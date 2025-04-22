bf1989c89e58b499d441b9139f84e791fbd12fde
Tom0Brien <thomas16441@gmail.com>
2025-04-22 19:21:16 +1000

    Enable persistent compilation cache

--- hydrax/__init__.py
+++ hydrax/__init__.py
@@ -1,8 +1,12 @@
 import os
 from pathlib import Path
+import jax
 
 # package root
 ROOT = str(Path(__file__).parent.absolute())
 
 # Set XLA flags for better performance
 os.environ["XLA_FLAGS"] = "--xla_gpu_triton_gemm_any=true "
+
+# Enable persistent compilation cache
+jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")
