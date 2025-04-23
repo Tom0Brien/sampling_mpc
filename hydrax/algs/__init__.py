cdd75c61c395aa84da10d80eddcb533715912897
Tom0Brien <thomas16441@gmail.com>
2025-04-23 13:11:32 +1000

    Include hydrax as a submodule

--- hydrax/algs/__init__.py
+++
@@ -1,7 +0,0 @@
-from .cem import CEM
-from .evosax import Evosax
-from .mppi import MPPI
-from .predictive_sampling import PredictiveSampling
-from .ccem import CCEM
-
-__all__ = ["CEM", "MPPI", "PredictiveSampling", "Evosax", "CCEM"]
